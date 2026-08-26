0fc8b772af174911dfdc108a117d77b5f0740137
yashbansal-dev <yashbansal531@gmail.com>
2026-08-26 10:16:31 +0000

    Deploying to gh-pages from @ yashbansal-dev/portfolio01@c1934fccb8161725ca571301d26612eb715e0315 🚀

--- _next/static/MNm4LiCnGb_XfJh3-zEd9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-6cf11adc33c7fdd4.js","static/chunks/921-f03f0ca0b1746e3a.js","static/chunks/pages/index-552d7cb64d1ad6c0.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
